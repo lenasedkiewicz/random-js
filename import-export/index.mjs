7fb2c3adc59e2b7473b6d262469c9eb0a5f6e1b3
msedkiewicz <magdalenainspirations@gmail.com>
2024-12-29 18:37:36 +0100

    Add days to flight function and data

--- import-export/index.mjs
+++ import-export/index.mjs
@@ -1,11 +1,13 @@
-import calculateAge from "./utils/dateUtils.mjs";
+import calculateAge, {
+  daysToDeparture as daysToFlight,
+} from "./utils/dateUtils.mjs";
 
 const PASSENGER_INFO = [
   {
     personName: "Kacper",
     surname: "Sokołowski",
     birthday: new Date("1992-10-01"),
-    flightDate: new Date("2023-10-05T03:24:00"),
+    flightDate: new Date("2025-10-05T03:24:00"),
     seat: 21,
     seatDesignation: "A",
     aircraftManufacturer: 0,
@@ -14,17 +16,23 @@ const PASSENGER_INFO = [
     personName: "Filip",
     surname: "Mamcarczyk",
     birthday: new Date("1993-09-10"),
-    flightDate: new Date("2023-12-01T15:27:00"),
+    flightDate: new Date("2025-12-01T15:27:00"),
     seat: 37,
     seatDesignation: "B",
     aircraftManufacturer: 1,
   },
 ];
 
-function renderSinglePassengerInfo({ personName, surname, birthday }) {
+function renderSinglePassengerInfo({
+  personName,
+  surname,
+  birthday,
+  flightDate,
+}) {
   return {
     "Imię i nazwisko": `${personName} ${surname}`,
     Wiek: calculateAge(birthday),
+    "Dni do wylotu": daysToFlight(flightDate),
   };
 }
 
--- import-export/utils/dateUtils.mjs
+++ import-export/utils/dateUtils.mjs
@@ -2,3 +2,8 @@ export default function calculateAge(birthday) {
   const now = new Date();
   return now.getFullYear() - birthday.getFullYear();
 }
+
+export function daysToDeparture(date) {
+  const now = new Date();
+  return Math.ceil((date.getTime() - now.getTime()) / (1000 * 3600 * 24));
+}
